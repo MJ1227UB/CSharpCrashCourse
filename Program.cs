27ea34022d1f35f4d6e0ad1408523648238c1ea3
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-08-14 12:00:59 +0200

    Made the first 8 exercises

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpCrashDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+
+            //Console.WriteLine("Hello Mathias!");
+
+            //2
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to add:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //var Sum = FirstNumber + SecondNumber;
+            //Console.WriteLine($"{FirstNumber} + {SecondNumber} = {Sum}");
+
+            //3
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to divide:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //var Sum = FirstNumber / SecondNumber;
+            //Console.WriteLine($"{FirstNumber} / {SecondNumber} = {Sum}");
+
+            //4
+
+            //var Sum1 = -1 + 4 * 6;
+            //var Sum2 = (35 + 5) % 7;
+            //var Sum3 = 14 + -4 * 6 / 11;
+            //var Sum4 = 2 + 15 / 6 * 1 - 7 % 2;
+            //Console.WriteLine($"{Sum1} \n{Sum2} \n{Sum3} \n{Sum4}");
+
+            //5
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to swap:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //var StoredNumber = FirstNumber;
+            //FirstNumber = SecondNumber;
+            //SecondNumber = StoredNumber;
+            //Console.WriteLine($"First Number : {FirstNumber} \nSecond Number: {SecondNumber}");
+
+            //6
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to multiply:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type another number to multiply");
+            //var ThirdNumber = int.Parse(Console.ReadLine());
+
+            //var Sum = FirstNumber * SecondNumber * ThirdNumber;
+            //Console.WriteLine($"{FirstNumber} * {SecondNumber} * {ThirdNumber} = {Sum}");
+
+            //7
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type another number:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+
+            //var ResultPlus = FirstNumber + SecondNumber;
+            //var ResultMinus = FirstNumber - SecondNumber;
+            //var ResultMultiply = FirstNumber * SecondNumber;
+            //var ResultDivide = FirstNumber / SecondNumber;
+            //var ResultMod = FirstNumber % SecondNumber;
+
+            //Console.WriteLine($"{FirstNumber} + {SecondNumber} = {ResultPlus}" +
+            //                  $"\n{FirstNumber} - {SecondNumber} = {ResultMinus}" +
+            //                  $"\n{FirstNumber} x {SecondNumber} = {ResultMultiply}" +
+            //                  $"\n{FirstNumber} / {SecondNumber} = {ResultDivide}" +
+            //                  $"\n{FirstNumber} mod {SecondNumber} = {ResultMod}");
+
+            //8
+
+            //Console.WriteLine("Type a number:");
+            //var Number = int.Parse(Console.ReadLine());
+            //for (int i = 0; i >= 10; i++)
+            //{
+            //    var Sum = Number * i;
+            //    Console.WriteLine(Sum);
+            //}
+
+            //9
+
+
+
+            //Shutdown
+            Console.ReadLine();
+        }
+    }
+}
